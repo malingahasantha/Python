225454b1e3f8ceb1a3d8314a07e425f60bf2b64b
malingahasantha <malinga.hasantha@gmail.com>
2024-07-26 09:32:03 +0530

    Update Readme file

--- 4_dynamically_type.py
+++ 4_dynamically_type.py
@@ -8,4 +8,4 @@ x = 'Garg Studio'
 print(type(x))
 
 # as a example Java is a Statically Type Language. We cannot change a defined variable value in runtime. Also we should provide a value when we define the variable.
-# But in Python we can change the defined variable value in runtime. So Python is a Dynamicall Type language.
+# But in Python we can change a defined variable value in runtime. So Python is a Dynamicall Type language.
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Python
 Python Learning and Practice
+
+### hello
+We start with hello world
+
+### variables
+Python variables Types. Practice varibale types with examples.
+
+### data types
+Python data types. Python data types with examples. integer, float, strings...
+
+### dynamically type
+Python is a dynamically type language. we can change a defined variable value in runtime.
