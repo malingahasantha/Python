9f2f5283c3352420ee822ea4ff6d68e037cbe28e
malingahasantha <malinga.hasantha@gmail.com>
2024-08-10 21:21:57 +0530

    duplicates values interview question

--- Interview_Questions/q2.py
+++ Interview_Questions/q2.py
@@ -5,7 +5,7 @@ For example: input: [1,2,3,1,3,6,5] output:[1,3]
 
 import array
 
-arr1 = [1,2,3,1,3,6,5,5,5]
+arr1 = [1,2,3,1,3,6,5,5,5,8,8]
 
 unique=[]
 duplicates=[]
@@ -18,3 +18,29 @@ for num in arr1:
 
 print(duplicates)   
 print(unique)  
+
+
+print("\n----------------------- method 2 -------------------------------------")
+
+
+def find_duplicates(input_arr):
+    counts={}       #create empty dictionary
+    duplicates=[]   #create empty list
+
+    for num in input_arr:   #for loop to check array, num--> is the variable assigning array elements
+        if num in counts:
+            counts[num] += 1 #if counts[num] is in counts dictionary, it's value increase by 1
+        else:
+            counts[num] = 1
+        #dictionary output --> {1:2, 2:1, 3:2, 6:1, 5:3, 8:2}
+    
+    for key, value in counts.items():
+        if value > 1:
+            duplicates.append(key)
+    
+    return duplicates
+    
+result = find_duplicates(arr1)
+print(result)
+
+
