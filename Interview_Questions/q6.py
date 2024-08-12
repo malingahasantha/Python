f5665c9b1a041e215fe97a4ac67ea23c4011bb00
malingahasantha <malinga.hasantha@gmail.com>
2024-08-12 10:11:47 +0530

    left rotation of a list

--- Interview_Questions/q6.py
+++ Interview_Questions/q6.py
@@ -19,3 +19,17 @@ def left_rotate(il,rn):
 
 rotated_list = left_rotate(input_list,rotate_number)
 print(rotated_list)
+
+
+print("\n------------------------- 2nd method -------------------------")
+
+def left_rotation(l,n):
+    for i in range(n):      # range n means 0:1 --> 0 to 1
+        t = l[0]            # t = 100
+        for j in range(len(l)-1):  # 0,1,2,3 --> 0:4 0 to 4
+            l[j] = l[j+1]   # l[0] = l[1], l[1] = l[2], l[2] = l[3], l[3] = l[4]
+            l[len(l)-1] = t # l[4] = 100 --> 200 --> in 2nd rotation t becoms 200
+    return l
+
+rotated_left = left_rotation(input_list,rotate_number)
+print(rotated_left)
