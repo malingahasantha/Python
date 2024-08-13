c60033bf4418372e1f526a0d1dc8f614907f0aa1
malingahasantha <malinga.hasantha@gmail.com>
2024-08-13 10:48:41 +0530

    print even and odd number

---
+++ Interview_Questions/q8.py
@@ -0,0 +1,12 @@
+"""
+Q8. Write a program to get user input and print Even or Odd?
+"""
+
+user_input = int(input("Enter a number: "))
+
+if user_input % 2 == 0:
+    print("You Entered", user_input ,", It is an Even Number")
+else:
+    print("You Entered", user_input ,", It is an Odd Number")
+
+
