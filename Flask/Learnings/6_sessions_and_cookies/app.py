bbf5ee0e77508323029089f87bc7f2c3b64c4110
malingahasantha <malinga.hasantha@gmail.com>
2024-09-06 09:52:22 +0530

    cookies, set get and remove cookies

--- Flask/Learnings/6_sessions_and_cookies/app.py
+++ Flask/Learnings/6_sessions_and_cookies/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, make_response, request
 
 app = Flask(__name__, template_folder = 'templates')
 app.secret_key = 'SOME KEY'
@@ -7,11 +7,13 @@ app.secret_key = 'SOME KEY'
 def index():
     return render_template('index.html', message='index')
 
+
 @app.route('/set_data')
 def set_data():
     session['name'] = 'Hasantha'
     session['other'] = 'Hello World'
     return render_template('index.html', message='Session data set')
+
 
 @app.route('/get_data')
 def get_data():
@@ -22,10 +24,32 @@ def get_data():
     else: 
         return render_template('index.html', message=f'No Session Found')
 
+
 @app.route('/clear_session')
 def clear_session():
     session.clear()
     return render_template('index.html', message='Session Cleared')
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response(render_template('index.html', message='Cookie set'))
+    response.set_cookie('cookie_name', 'cookie_value')
+    return response
+
+
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies['cookie_name']
+    return render_template('index.html', message=f'Cookie Value: {cookie_value}')
+
+
+@app.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template('index.html', message='Cookie Removed'))
+    response.set_cookie('cookie_name', expires=0)
+    return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
