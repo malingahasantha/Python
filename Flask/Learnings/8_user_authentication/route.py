1b85d5d6f843732fa0b6a99c2d21de517df965b1
malingahasantha <malinga.hasantha@gmail.com>
2024-09-08 21:00:15 +0530

    uaer authentication without adding any users

---
+++ Flask/Learnings/8_user_authentication/route.py
@@ -0,0 +1,23 @@
+from flask import render_template, request
+from flask_login import login_user, logout_user, current_user, login_required
+
+from models import User
+
+def register_routes(app, db, bcrypt):
+
+    @app.route('/', methods=['GET','POST'])
+    def index(): 
+        if current_user.is_authenticated == True:
+            return str(current_user.username)
+        else:
+            return 'No user is logged in'
+    
+    @app.route('/login/<uid>')
+    def login(uid):
+        login_user(uid)
+        return 'Success'
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return 'Success'
